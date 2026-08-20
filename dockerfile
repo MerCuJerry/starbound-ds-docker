94d234061af30d1f1af9411fe8474546d0fb6ebc
MerCuJerry <mercujerry@gmail.com>
2026-08-20 10:50:30 +0800

    update

--- dockerfile
+++ dockerfile
@@ -1,20 +1,17 @@
 FROM cm2network/steamcmd:root-trixie
 
-ENV STEAM_ACCOUNT account
-ENV STEAM_PASSWORD password
-ENV MOD_IDS ()
-ENV STEAMAPPDIR "${HOMEDIR}/starbound"
-ENV PUID 1000
-ENV SKIP_INSTALL false
+ENV STEAM_ACCOUNT=account
+ENV STEAM_PASSWORD=password
+ENV MOD_IDS=()
+ENV STEAMAPPDIR="${HOMEDIR}/starbound"
+ENV SKIP_INSTALL=false
+ENV PUID=1000
+ENV PGID=1000
 
 ADD install.sh $HOMEDIR/install.sh
 RUN mkdir -p $STEAMAPPDIR
 
-USER root
-RUN chown -R "${PUID}:${PUID}" "${HOMEDIR}"
-
-USER $PUID:$PUID
-RUN chmod u+x $HOMEDIR/install.sh
+RUN chmod +x $HOMEDIR/install.sh
 RUN touch $HOMEDIR/installmods.txt
 
 WORKDIR ${HOMEDIR}
--- install.sh
+++ install.sh
@@ -1,49 +1,60 @@
 #!/bin/bash
+# Setting UID AND GID
+if [ ! -z "$PUID" ] && [ "$PUID" != "$(id -u steam)" ]; then
+    usermod -o -u "$PUID" steam
+fi
+if [ ! -z "$PGID" ] && [ "$PGID" != "$(id -g steam)" ]; then
+    groupmod -o -g "$PGID" steam
+fi
+
+chown -R steam:steam ${HOMEDIR}
+
+su - steam
 #Install the Starbound dedicated server
 if ${SKIP_INSTALL} != "false"
 then
   echo "Skipping steamcmd installation and update"
 else
   echo "Installing and updating Starbound dedicated server with steamcmd"
-  ${STEAMCMDDIR}/steamcmd.sh \
+  su - steam -c "${STEAMCMDDIR}/steamcmd.sh \
       +force_install_dir ${STEAMAPPDIR}/ \
       +login ${STEAM_ACCOUNT} ${STEAM_PASSWORD} \
       +app_update 211820 validate \
-      +quit
+      +quit"
 
   chmod u+x ${STEAMAPPDIR}/linux/starbound_server
 
   #Build the mod install script
-  echo force_install_dir ${STEAMAPPDIR}/ >> ${STEAMAPPDIR}/installmods.txt
-  echo login ${STEAM_ACCOUNT} ${STEAM_PASSWORD} >> ${STEAMAPPDIR}/installmods.txt
+  su - steam -c "echo force_install_dir ${STEAMAPPDIR}/ >> ${STEAMAPPDIR}/installmods.txt"
+  su - steam -c "echo login ${STEAM_ACCOUNT} ${STEAM_PASSWORD} >> ${STEAMAPPDIR}/installmods.txt"
 
   rm ${STEAMAPPDIR}/mods/*
   rm -R ${STEAMAPPDIR}/steamapps
 
   for mod_id in ${MOD_IDS}
   do
-    echo workshop_download_item 211820 $mod_id >> ${STEAMAPPDIR}/installmods.txt
+    su - steam -c "echo workshop_download_item 211820 $mod_id >> ${STEAMAPPDIR}/installmods.txt"
   done
 
-  echo quit >> ${STEAMAPPDIR}/installmods.txt
+  su - steam -c "echo quit >> ${STEAMAPPDIR}/installmods.txt"
 
   #install Mods
-  ${STEAMCMDDIR}/steamcmd.sh +runscript ${STEAMAPPDIR}/installmods.txt
+  su - steam -c "${STEAMCMDDIR}/steamcmd.sh +runscript ${STEAMAPPDIR}/installmods.txt"
 
   #Move all content.pak files from each mod to the mods folder and rename them
   for mod_id in ${MOD_IDS}
   do
     if [ -f ${STEAMAPPDIR}/steamapps/workshop/content/211820/$mod_id/contents.pak ]
     then
-      mv ${STEAMAPPDIR}/steamapps/workshop/content/211820/$mod_id/contents.pak ${STEAMAPPDIR}/mods/$mod_id.pak
+      su - steam -c "mv ${STEAMAPPDIR}/steamapps/workshop/content/211820/$mod_id/contents.pak ${STEAMAPPDIR}/mods/$mod_id.pak"
     else
-      mv -v ${STEAMAPPDIR}/steamapps/workshop/content/211820/$mod_id/* ${STEAMAPPDIR}/mods/
+      su - steam -c "mv -v ${STEAMAPPDIR}/steamapps/workshop/content/211820/$mod_id/* ${STEAMAPPDIR}/mods/"
     fi
   done
 fi
 
 #Run the server
 cd ${STEAMAPPDIR}/linux
-./starbound_server
+su - steam -c "./starbound_server"
 
 exit
